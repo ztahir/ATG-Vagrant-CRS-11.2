095651de27dbe79f68e3a84497064ae7486c3df9
Zeeshan Ahmad Tahir <ztahir@gmail.com>
2018-12-10 16:27:20 +0400

    changing the module list

--- scripts/atg/provision_install_crs_artifacts.sh
+++ scripts/atg/provision_install_crs_artifacts.sh
@@ -7,8 +7,8 @@ exec sudo -u vagrant /bin/bash -l << eof
 	cp /vagrant/scripts/atg/crs_artifacts/ATGProduction.xml /vagrant/scripts/atg/crs_artifacts/ATGPublishing.xml /home/vagrant/jboss/standalone/configuration
 
 	# create jboss deployments
-	/home/vagrant/ATG/ATG11.3/home/bin/runAssembler -server "ATGProduction" -jboss "/home/vagrant/jboss/standalone/deployments/ATGProduction/ATGProduction.ear" -m Store.EStore DafEar.Admin DPS DSS ContentMgmt DCS.PublishingAgent DCS.AbandonedOrderServices ContentMgmt.Endeca.Index DCS.Endeca.Index Store.Endeca.Index DAF.Endeca.Assembler PublishingAgent DCS.Endeca.Index.SKUIndexing Store.Storefront Store.Recommendations DCS.ClickToConnect Store.Fluoroscope Store.Fulfillment Store.KnowledgeBase
-	/home/vagrant/ATG/ATG11.3/home/bin/runAssembler -server "ATGPublishing" -jboss "/home/vagrant/jboss/standalone/deployments/ATGPublishing/ATGPublishing.ear" -m DCS-UI.Versioned BIZUI PubPortlet DafEar.Admin ContentMgmt.Versioned DCS-UI.SiteAdmin.Versioned SiteAdmin.Versioned DCS.Versioned DCS-UI Store.EStore.Versioned Store.Storefront ContentMgmt.Endeca.Index.Versioned DCS.Endeca.Index.Versioned Store.Endeca.Index.Versioned DCS.Endeca.Index.SKUIndexing Store.KnowledgeBase 
+/home/vagrant/ATG/ATG11.3/home/bin/runAssembler -server "ATGProduction" -jboss "/home/vagrant/jboss/standalone/deployments/ATGProduction/ATGProduction.ear" -m Store.EStore.International DCS.AbandonedOrderServices DafEar.Admin DPS DSS ContentMgmt DCS.PublishingAgent DCS.AbandonedOrderServices ContentMgmt.Endeca.Index DCS.Endeca.Index DCS.Endeca.Assembler Store.Endeca.Index Store.Endeca.Assembler DAF.Endeca.Assembler Store.WCSExtensions Store.WCSExtensions.Endeca.Index PublishingAgent REST.JAXRSPublic.Version1 DCS.Endeca.Index.SKUIndexing Store.Storefront Store.EStore.International Store.Recommendations Store.Mobile Store.Endeca.International Store.WCSExtensions.International Store.WCSExtensions.Endeca.International Store.Fluoroscope Store.Fulfillment Store.KnowledgeBase.International Store.Mobile.REST Store.Mobile.Recommendations Store.Mobile.International REST.base REST.Actor Store.Recommendations.International
+/home/vagrant/ATG/ATG11.3/home/bin/runAssembler -server "ATGPublishing" -jboss "/home/vagrant/jboss/standalone/deployments/ATGPublishing/ATGPublishing.ear" -m DCS-UI.Versioned BIZUI PubPortlet DafEar.Admin ContentMgmt.Versioned DCS-UI.SiteAdmin.Versioned SiteAdmin.Versioned DCS.Versioned DCS-UI Store.EStore.Versioned Store.Storefront ContentMgmt.Endeca.Index.Versioned DCS.Endeca.Index.Versioned DCS.Endeca.Assembler.Versioned Store.Endeca.Index.Versioned Store.Endeca.Assembler.Versioned DAF.Endeca.Reader.Versioned Store.Endeca.Reader Store.WCSExtensions.Versioned Store.WCSExtensions.Endeca.Index.Versioned WCSExtensions.Endeca.Reader.Versioned DCS.Endeca.Index.SKUIndexing Store.EStore.International.Versioned Store.Mobile Store.Mobile.Versioned Store.Endeca.International Store.Endeca.International.Reader Store.WCSExtensions.International.Versioned Store.WCSExtensions.Endeca.International Store.KnowledgeBase.International Store.Mobile.REST.Versioned Store.Mobile.International.Versioned
 
 	# make sure jboss knows to deploy
 	touch /home/vagrant/jboss/standalone/deployments/ATGProduction/ATGProduction.ear.dodeploy
@@ -24,8 +24,6 @@ exec sudo -u vagrant /bin/bash -l << eof
 	# initialize the CRS Eac artifacts
 	cp -r /home/vagrant/ATG/ATG11.3/CommerceReferenceStore/Store/Storefront/deploy /home/vagrant/deploy
 	/usr/local/endeca/ToolsAndFrameworks/11.3.1.5.0/deployment_template/bin/deploy.sh --app /home/vagrant/deploy/deploy.xml < /vagrant/scripts/atg/deploy_CRS_endeca_silent.silentinput
-    /vagrant/scripts/atg/stop_endeca_services.sh 
-	/vagrant/scripts/atg/start_endeca_services.sh
 	/usr/local/endeca/Apps/CRS/control/initialize_services.sh --force
 
 
